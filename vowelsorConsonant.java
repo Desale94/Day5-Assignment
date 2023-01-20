7cb741016714c040b50c9ef40775e44e8c5dd205
Desale1 <dkdesale08@gmail.com>
2023-01-20 08:15:18 +0530

    Check the alphabet is vowels or consonant

---
+++ vowelsorConsonant.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.demo;
+
+import java.util.Scanner;
+
+public class vowelsorConsonant {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the character: ");
+        char ch= sc.next().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ){
+            System.out.println("The character is Vowel");
+        }
+        else {
+            System.out.println("The character is Consonant");
+        }
+
+    }
+}
