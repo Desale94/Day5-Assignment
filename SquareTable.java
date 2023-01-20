b5eb7a826e9889dafdf0ad25e5aa919ee0f809b3
Desale1 <dkdesale08@gmail.com>
2023-01-20 09:26:25 +0530

    prints a table of the of power of 2 using command line

--- SquareTable.java
+++ SquareTable.java
@@ -1,13 +1,8 @@
 package com.bridgelabz.demo;
-import java.util.Scanner;
-
 public class SquareTable {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int num = sc.nextInt();
-
+        int num = Integer.parseInt(args[0]);
         if (num <= 0 || num < 31) {
             System.out.println("Table of power of 2");
             for (int i = 1; i <= num; i++) {
